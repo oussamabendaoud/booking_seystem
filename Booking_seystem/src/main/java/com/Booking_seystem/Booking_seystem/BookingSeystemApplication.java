83903731edc168873b9ec7fa4281f6378690a31a
oussamabendaoud <oussamabendaoud111@gmail.com>
2024-05-01 17:25:59 +0100

    Oussama v 1 Premier commit : Initialisation du projet

---
+++ Booking_seystem/src/main/java/com/Booking_seystem/Booking_seystem/BookingSeystemApplication.java
@@ -0,0 +1,13 @@
+package com.Booking_seystem.Booking_seystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSeystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSeystemApplication.class, args);
+	}
+
+}
---
+++ Booking_seystem/src/test/java/com/Booking_seystem/Booking_seystem/BookingSeystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Booking_seystem.Booking_seystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSeystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
